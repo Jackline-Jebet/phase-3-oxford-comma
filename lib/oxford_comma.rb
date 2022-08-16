fc89dcf49c191a43d336e3bd7bea5b7c586bcd1c
Jackline Jebet <jebetjackline2016@gmail.com>
2022-08-16 11:34:01 +0300

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    
+    if array.length >= 3 
+        arr= array.delete_at(array.length-1)
+        ("#{array.join(', ')}, and #{arr}" )
+    elsif array.length == 2
+         array.join(" and ")
+    elsif array.length == 1
+         array.join    
+    end         
 end
